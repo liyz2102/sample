37384bc96348e4a1daa5874076cfb434b085e718
vagrant <vagrant@homestead>
2017-12-13 12:04:54 +0000

    test

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>@yield('title','sample')</title>
+	<title>@yield('title','sample') - laravel study</title>
+	<link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
-	@yield('content')
+  <header class="navbar navbar-fix-top navbar -inverse">
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        <a href="/" id="logo">Sample App</a>
+        <nav>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/help">help</a></li>>
+            <li><a href="#">login</a></li>
+          </ul>
+        </nav>
+      </div>
+     </div>
+   </header>
+   
+	<div class="container">        
+	 @yield('content')
+	</div>
 </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,7 +1,20 @@
 
 
 @extends('layouts.default')
-@section('title','home page')
+
+@section('content')
+	<div class="jumbotron">
+	 	<h1>Hello Everyone</h1>
+	 	<p class="lead">
+	 	I'm lee !You are seeing...<a href="https://fsdhub.com/books/laravel-essential-training-5.1">laravel project</a>
+	 	</p>
+	 	<p>
+	 	let's begin
+	 	</p>
+	 	<p>
+	 		<a class="btn btn-lg btn-success" href="#" role="button">login</a>
+	 	</p>
+	</div>
 @section('content')
 	<h1>home<h1>
 @stop
