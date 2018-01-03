5cae3e6f906e29c1218fba9b8cf4dcdca294f570
vagrant <vagrant@homestead>
2018-01-03 10:32:10 +0000

    test

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,7 +1,7 @@
 
-<a herf="{{ route('users.show',$user->id) }}">
+<!-- <a herf="{{ route('users.show',$user->id) }}"> -->
 	<img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
 	
-</a>
+<!-- </a> -->
 
-<h1> {{ $user->name }} </h1>
+<h1> {{ $user->name }} -- {{ $user->email }}</h1>
